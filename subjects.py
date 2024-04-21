ba58ebc2ffbfb89c1fd1cab336c981d672ed8ad6
Adrien Schneider <adrien.schneider-noury@netc.fr>
2024-04-21 18:49:16 +0200

    Initial commit

---
+++ subjects.py
@@ -0,0 +1,13 @@
+subject_dict = {
+    "NUMERIQUE SC.INFORM.": ["NSI", "🖥️"],
+    "MATHEMATIQUES": ["Maths Spé.", "🔢"],
+    "ALLEMAND LV2": ["Allemand", "🇩🇪"],
+    "ANGLAIS LV1": ["Anglais", "🇬🇧"],
+    "HISTOIRE-GEOGRAPHIE": ["Histoire-Géo", "🏛️"],
+    "ENSEIGN.SCIENTIFIQUE": ["Ens. Sci.", "⚗️"],
+    "PHILOSOPHIE": ["Philosophie", "💭"],
+    "VIE DE CLASSE": ["Vie de Classe", "ℹ️"],
+    "MATHS EXPERTES": ["Maths Exp.", "🔣"],
+    "ED.PHYSIQUE & SPORT.": ["EPS", "👟"],
+    "ENS. MORAL & CIVIQUE": ["EMC", "⚖️"]
+}
